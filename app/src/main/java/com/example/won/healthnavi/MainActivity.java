9fe3752f18b0f003b20141e493dbc266e4c60e69
WON <aekik1118@naver.com>
2018-11-13 22:01:27 +0900

    Login 기능 firebase 기반 구현

--- app/src/main/java/com/example/won/healthnavi/MainActivity.java
+++ app/src/main/java/com/example/won/healthnavi/MainActivity.java
@@ -1,26 +1,101 @@
 package com.example.won.healthnavi;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseNetworkException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
 
-        Button button4 = findViewById(R.id.button4);
+        final EditText emailTxt = (EditText)findViewById(R.id.editText);
+        final EditText pwTxt = (EditText)findViewById(R.id.editText2);
+        Button LoginBtn = findViewById(R.id.button3);
+        Button signUpBtn = findViewById(R.id.button4);
 
-        button4.setOnClickListener(new View.OnClickListener() {
+        LoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailTxt.getText().toString();
+                String password = pwTxt.getText().toString();
+
+                loginStart(email,password);
+            }
+        });
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(),SignUpActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
     }
+
+    public void loginStart(String email, String password){
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Toast.makeText(MainActivity.this,"mAuth. onComplete 함수" ,Toast.LENGTH_SHORT).show();
+                if (!task.isSuccessful()) {
+                    try {
+                        throw task.getException();
+                    } catch (FirebaseAuthInvalidUserException e) {
+                        Toast.makeText(MainActivity.this,"존재하지 않는 id 입니다." ,Toast.LENGTH_SHORT).show();
+                    } catch (FirebaseAuthInvalidCredentialsException e) {
+                        Toast.makeText(MainActivity.this,"이메일 형식이 맞지 않습니다." ,Toast.LENGTH_SHORT).show();
+                    } catch (FirebaseNetworkException e) {
+                        Toast.makeText(MainActivity.this,"Firebase NetworkException" ,Toast.LENGTH_SHORT).show();
+                    } catch (Exception e) {
+                        Toast.makeText(MainActivity.this,"Exception" ,Toast.LENGTH_SHORT).show();
+                    }
+
+                }else{
+
+
+                    currentUser = mAuth.getCurrentUser();
+
+                    Toast.makeText(MainActivity.this, "로그인 성공" + "/" + currentUser.getEmail() + "/" + currentUser.getUid() ,Toast.LENGTH_SHORT).show();
+
+                    startActivity(new Intent(MainActivity.this, MainListActivity.class));
+                    finish();
+                }
+
+            }
+        });
+    }
+// 로그인 되어있을때 자동으로 메인화면 가기
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        currentUser = mAuth.getCurrentUser();
+//        if(currentUser != null){
+//            startActivity(new Intent(MainActivity.this, MainListActivity.class));
+//            finish();
+//        }
+//    }
 }
